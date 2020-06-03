b3009c634786cd92d0c925788b23fdbe7bc0e2ae
tylerweidel <tjw941@gmail.com>
2020-06-03 12:04:15 -0400

    Initial Commit

---
+++ FlatFormTextFieldDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlatFormTextFieldDemo
+//
+//  Created by Tyler Weidel on 6/3/20.
+//  Copyright © 2020 1SE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
